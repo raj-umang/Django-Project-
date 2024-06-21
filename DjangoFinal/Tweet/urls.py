fcdef31809f84792d280ede8fed74704a754098d
raj-umang <jackerumang@gmail.com>
2024-06-22 03:39:19 +0530

    login, logout & register

--- DjangoFinal/Tweet/urls.py
+++ DjangoFinal/Tweet/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('<int:tweet_id>/edit/', views.tweet_delete, name='tweet_delete'),
 
     path('<int:tweet_id>/delete/', views.tweet_edit, name='tweet_edit'),
+
+    path('register/', views.register, name='register'),
 ] 
